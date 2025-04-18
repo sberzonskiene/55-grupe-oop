b0da863397b3218a4e34d06516522dda11f64cd5
sberzonskiene <s.berzonskiene@gmail.com>
2025-04-18 11:28:35 +0300

    oop

--- gyvunai.js
+++ gyvunai.js
@@ -1,9 +1,51 @@
-class gyvunai {
+class animal {
     constructor(name, age, furColor, type, legsCount)
     {
-        this.name = 'Vudis';
-        this.age = 6;
-        this.furColor = 'black';
-        this.type = 'home pet';
+        this.name = name;
+        this.age = age;
+        this.furColor = furColor;
+        this.type = 'unknown';
+        this.legsCount = 0;
+    } 
+}
+
+class Dog extends animal {
+    constructor(name, age, furColor)
+    {
+        super(name, age, furColor);
+        this.type = 'pet';
         this.legsCount = 4;
-    } }
+    }
+}
+
+class Cat extends animal {
+    constructor(name, age, furColor)
+    {
+        super(name, age, furColor);
+        this.type = 'pet';
+        this.legsCount = 4;
+    }
+}
+
+class Bird extends animal {
+    constructor(name, age, furColor)
+    {
+        super(name, age, furColor);
+        this.type = 'wild';
+        this.legsCount = 2;
+    }
+}
+
+
+const vudis = new Dog("Vudis", 6, 'black');
+console.log(vudis);
+
+const pupa = new Cat('Pupa', 5, 'colorred');
+console.log(pupa);
+
+const zylute = new Bird('Zylutė', 3, 'yellow');
+console.log(zylute);
+
+const gyvunai = [vudis, pupa, new Dog('Šnapė', 10, "caramel"), new Bird('Žvirblis', 2, 'brown'), new Cat('Mauris', 7, 'gray')];
+console.log(gyvunai);
+
