e7c1f07f995c9ee47c01b446f9a992c38ef0939c
sberzonskiene <s.berzonskiene@gmail.com>
2025-04-18 13:15:43 +0300

    oop

--- gyvunai.js
+++ gyvunai.js
@@ -6,46 +6,53 @@ class animal {
         this.furColor = furColor;
         this.type = 'unknown';
         this.legsCount = 0;
+        this.voice = 'unknown';
+        this.behavior = behavior;
+        this.owner = owner;
+        
     } 
 }
 
 class Dog extends animal {
-    constructor(name, age, furColor)
+    constructor(name, age, furColor, behavior, owner)
     {
-        super(name, age, furColor);
+        super(name, age, furColor, behavior, owner);
         this.type = 'pet';
         this.legsCount = 4;
+        this.voice = 'loja';
     }
 }
 
 class Cat extends animal {
-    constructor(name, age, furColor)
+    constructor(name, age, furColor, behavior, owner)
     {
-        super(name, age, furColor);
+        super(name, age, furColor, behavior, owner);
         this.type = 'pet';
         this.legsCount = 4;
+        this.voice = 'kniaukia';
     }
 }
 
 class Bird extends animal {
-    constructor(name, age, furColor)
+    constructor(name, age, furColor, behavior, owner)
     {
-        super(name, age, furColor);
+        super(name, age, furColor, behavior, owner);
         this.type = 'wild';
         this.legsCount = 2;
+        this.voice = 'čiulba';
     }
 }
 
 
-const vudis = new Dog("Vudis", 6, 'black');
+const vudis = new Dog("Vudis", 6, 'black', 'angry', 'Kazimieras');
 console.log(vudis);
 
-const pupa = new Cat('Pupa', 5, 'colorred');
+const pupa = new Cat('Pupa', 5, 'colorred', 'cunning', 'Zenutė');
 console.log(pupa);
 
-const zylute = new Bird('Zylutė', 3, 'yellow');
+const zylute = new Bird('Zylutė', 3, 'yellow', 'happy', 'neturi');
 console.log(zylute);
 
-const gyvunai = [vudis, pupa, new Dog('Šnapė', 10, "caramel"), new Bird('Žvirblis', 2, 'brown'), new Cat('Mauris', 7, 'gray')];
+const gyvunai = [vudis, pupa, new Dog('Šnapė', 10, 'caramel', 'friendly', 'Kaziukas'), new Bird('Žvirblis', 2, 'brown', 'shy', 'neturi'), new Cat('Mauris', 7, 'gray', 'lazy', 'Zenutė')];
 console.log(gyvunai);
 
