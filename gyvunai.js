19c07373b3fd0453c05414c8efc28e17e89f82fc
sberzonskiene <s.berzonskiene@gmail.com>
2025-04-18 09:36:12 +0300

    oop

---
+++ gyvunai.js
@@ -0,0 +1,9 @@
+class gyvunai {
+    constructor(name, age, furColor, type, legsCount)
+    {
+        this.name = 'Vudis';
+        this.age = 6;
+        this.furColor = 'black';
+        this.type = 'home pet';
+        this.legsCount = 4;
+    } }
